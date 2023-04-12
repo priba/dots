9bcec42084ff8166cea3d47575f36ddc8b6c5114
Pau Riba <pau.riba@helsing.ai>
2023-04-13 01:06:45 +0200

    Working keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>") -- jk to escape
 
 -- general maps
 vim.keymap.set("i", "<C-k>", "<C-o>d$", { silent = true }) -- delete until end of line
+vim.keymap.set("n", "<C-k>", "d$", { silent = true }) -- delete until end of line
 vim.keymap.set("", "<leader>w", ":w<CR>", { silent = true }) -- save with leader`
 vim.keymap.set("", "<leader>qa", ":qall!<CR>", { silent = true }) -- quit all
 vim.keymap.set("", "<leader>qw", ":q<CR>", { silent = true }) -- quit (write as its save automatically)
@@ -13,27 +14,26 @@ vim.keymap.set("n", "<Tab>", ":bnext<CR>") -- Move to the next buffer
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>") -- Move to the previous buffer
 -- -- Moving between splits with arrow keys, sorry Vim, I don't deserve you :(
 -- -- Check this for MacOS: https://apple.stackexchange.com/questions/341993/macos-can-i-disable-f12-ctrl-left-arrow-ctrl-right-arrow 
-vim.keymap.set("n", "<C-Down>", "<C-W><C-J>")
-vim.keymap.set("n", "<C-Up>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-Left>", "<C-W><C-H>")
-vim.keymap.set("n", "<C-Right>", "<C-W><C-L>")
+-- vim.keymap.set("n", "<C-Down>", "<C-W><C-J>")
+-- vim.keymap.set("n", "<C-Up>", "<C-W><C-K>")
+-- vim.keymap.set("n", "<C-Left>", "<C-W><C-H>")
+-- vim.keymap.set("n", "<C-Right>", "<C-W><C-L>")
 
 -- copy pasting
 vim.keymap.set({ "n", "v" }, "<leader>y", "+y")
 
-
 -- Terminal Settings
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>`.$", { silent = true })
-vim.keymap.set("t", "<A-left>", "<C-\\><C-n><C-w>h")
-vim.keymap.set("t", "<A-down>", "<C-\\><C-n><C-w>j")
-vim.keymap.set("t", "<A-up>", "<C-\\><C-n><C-w>k")
-vim.keymap.set("t", "<A-right>", "<C-\\><C-n><C-w>l")
+-- vim.keymap.set({"n", "t"}, "<Esc>", "<C-Bslash><C-n>`.$", { silent = true })
+vim.keymap.set({"n", "t"}, "<A-left>", "<C-Bslash><C-n><C-w>h")
+vim.keymap.set({"n", "t"}, "<A-down>", "<C-Bslash><C-n><C-w>j")
+vim.keymap.set({"n", "t"}, "<A-up>", "<C-Bslash><C-n><C-w>k")
+vim.keymap.set({"n", "t"}, "<A-right>", "<C-Bslash><C-n><C-w>l")
 
-vim.cmd("autocmd BufEnter term://* startinsert")
+-- vim.cmd("autocmd BufEnter term://* startinsert")
 
 -- Move current line up and down
-vim.keymap.set("n", "<A-s>", ":m .+1<CR>==")
-vim.keymap.set("n", "<A-w>", ":m .-2<CR>==")
+vim.keymap.set("n", "<A-s>", ":m .+1<CR>==", {noremap=true})
+vim.keymap.set("n", "<A-w>", ":m .-2<CR>==", {noremap=true} )
 vim.keymap.set("i", "<A-s>", "<Esc>:m .+1<CR>==gi")
 vim.keymap.set("i", "<A-w>", "<Esc>:m .-2<CR>==gi")
 vim.keymap.set("v", "<A-s>", ":m '>+1<CR>gv=gv")
@@ -43,7 +43,9 @@ vim.keymap.set("v", "<A-w>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<leader>se", "<ESC>^vg_")
 vim.keymap.set("n", "<leader>se", "^vg_")
 
-vim.keymap.set("n", "<leader>)", 'ciw(<c-r>")<ESC>') -- Surround with parenthesis
+vim.keymap.set("n", "<leader>)", "ciw(<c-r>\")<ESC>") -- Surround with parenthesis
+vim.keymap.set("n", "<leader>]", "ciw[<c-r>\"]<ESC>") -- Surround with parenthesis
+vim.keymap.set("n", "<leader>}", "ciw{<c-r>\"}<ESC>") -- Surround with parenthesis
 
 -- Delete word forward, counter of C-W in insert  
 vim.keymap.set("i", "<C-q>", "<C-o>dw")
