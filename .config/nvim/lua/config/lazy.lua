c1f1f657d87e86ed48413f6ac584e747bb455da8
Pau Riba <pau.riba@helsing.ai>
2023-04-12 21:23:49 +0200

    Add nvim init

---
+++ .config/nvim/lua/config/lazy.lua
@@ -0,0 +1,40 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    import = "plugins"
+  },
+  defaults = {
+    lazy = true
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        -- "matchit",
+        "netrwPlugin",
+        "gzip",
+        -- "matchparen",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+require("config.settings")
+require("config.keymaps")
+require("config.plugin_keymaps")
